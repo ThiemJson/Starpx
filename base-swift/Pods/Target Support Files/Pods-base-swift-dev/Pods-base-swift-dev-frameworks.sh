0832284c28992b2edeca0a3b4c4e1099c0971e31
thiemnc.rikkeisoft <thiemnc@rikkeisoft.com>
2024-05-27 17:20:34 +0700

    Implement Shared Extension to show Image dimension

--- base-swift/Pods/Target Support Files/Pods-base-swift-dev/Pods-base-swift-dev-frameworks.sh
+++ base-swift/Pods/Target Support Files/Pods-base-swift-dev/Pods-base-swift-dev-frameworks.sh
@@ -176,6 +176,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSAuthCore/AWSAuthCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProviderASF/AWSCognitoIdentityProviderASF.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSMobileClient/AWSMobileClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSPinpoint/AWSPinpoint.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
@@ -196,6 +202,12 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Toast-Swift/Toast_Swift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSAuthCore/AWSAuthCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProvider/AWSCognitoIdentityProvider.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCognitoIdentityProviderASF/AWSCognitoIdentityProviderASF.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSCore/AWSCore.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSMobileClient/AWSMobileClient.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/AWSPinpoint/AWSPinpoint.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CryptoSwift/CryptoSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Differentiator/Differentiator.framework"
--- base-swift/StarpxShareExt/ShareViewController.swift
+++ base-swift/StarpxShareExt/ShareViewController.swift
@@ -8,24 +8,76 @@
 
 import UIKit
 import Social
+import UniformTypeIdentifiers
 
 class ShareViewController: SLComposeServiceViewController {
-
+    
     override func isContentValid() -> Bool {
         // Do validation of contentText and/or NSExtensionContext attachments here
         return true
     }
-
+    
     override func didSelectPost() {
         // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
-    
+        
         // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
-        self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
-    }
+        //sometimes the item is a UIImage (i.e., screenshots)
+        if let extensionItems = self.extensionContext?.inputItems as? [NSExtensionItem]  {
+            let attachments     = extensionItems.first?.attachments ?? []
+            let imageType       = UTType.image.identifier
+            var imagesURL       : [URL] = [URL]()
+            var imagesSize      : [CGSize] = [CGSize]()
+            var count = 0
+            
+            let dispatchGroup = DispatchGroup()
+            
+            for provider in attachments {
+                if provider.hasItemConformingToTypeIdentifier(imageType) {
+                    count += 1
+                    print("It is an image")
+                    
+                    dispatchGroup.enter()
+                    // this seems only to handle media from photos
+                    provider.loadItem(forTypeIdentifier: imageType) { (unsafeFileUrl, error) in
+                        print("We have the image")
+                        if let url = unsafeFileUrl as? URL {
+                            imagesURL.append(url)
+                            if let source = CGImageSourceCreateWithURL(url as CFURL, nil) {
+                                let propertiesOptions = [kCGImageSourceShouldCache: false] as CFDictionary
+                                if let properties = CGImageSourceCopyPropertiesAtIndex(source, 0, propertiesOptions) as? [CFString: Any] {
+                                    if let width = properties[kCGImagePropertyPixelWidth] as? CGFloat,
+                                       let height = properties[kCGImagePropertyPixelHeight] as? CGFloat {
+                                        imagesSize.append(CGSize(width: width, height: height))
+                                    }
+                                }
+                            }
+                        }
+                        dispatchGroup.leave()
+                    }
+                }
+            }
+            
+            dispatchGroup.notify(queue: DispatchQueue.main) {
+                // create the actual alert controller view that will be the pop-up
+                let message = "URL Shared: \(imagesURL.count) \nImages shared: \(imagesSize.count)\n Dimentsion: \(imagesSize)"
+                let alertController = UIAlertController(title: "StarPX", message: message, preferredStyle: .alert)
+                // add the buttons/actions to the view controller
+                let close = UIAlertAction(title: "Close", style: .default) { _ in
+                    // this code runs when the user hits the "save" button
+                    let inputName = alertController.textFields![0].text
+                    self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+                }
 
+                alertController.addAction(close)
+
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
+    
     override func configurationItems() -> [Any]! {
         // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
         return []
     }
-
+    
 }
